9d0aa35a5f0800b814eab6f13e174214c2cdb9d5
tudousha <166604729+tudousha@users.noreply.github.com>
2025-12-20 23:41:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
     base: './',
     const env = loadEnv(mode, '.', '');
